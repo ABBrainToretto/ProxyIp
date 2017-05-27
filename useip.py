c197104ea58d15e1a06012270ddf0072eb4c9b31
ABBrainToretto <644853947@qq.com>
2017-05-27 12:07:43 +0800

    Create useip.py
    每次调用都有一个随机代理ip从数据库抽取出来，实现与真正爬虫requests的需求，下一版将会增加优化程序-----在ippool.py验证通过后，再在在数据库中循环验证ip的可用性。

---
+++ useip.py
@@ -0,0 +1,36 @@
+# _*_coding:utf-8 _*_
+import MySQLdb
+import requests
+
+class Sql():
+    def __init__(self):
+        self.conn = MySQLdb.connect(
+                host = "localhost",
+                port = 3306,
+                user = "root",
+                passwd = "123321",
+                db = "book",
+                charset = "utf8"
+            )
+        self.cur = self.conn.cursor()
+        
+    def mysqldata(self):
+        sql = "select * from ip_proxy ORDER BY RAND() LIMIT 1"
+        # sql = "insert into ip_proxy (`id`,`type`,`ip_proxy`)values(0,%s,%s)"
+        # param = (type,proxy)
+        self.cur.execute(sql)
+        # print self.cur.fetchall()
+        n = self.cur.fetchall()[0]
+        # print n
+        # for row in self.cur.fetchall():
+            # print row[1],row[2]
+        proxy = {}
+        proxy[n[1]] = n[2]
+        # print '您现在正在使用的代理是：',proxy
+        # self.conn.commit()
+        return proxy
+        
+if __name__=='__main__':
+    mysql = Sql()
+    mysql.mysqldata()
+
